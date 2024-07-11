837624b14b7705267870044bee7ec9bf00bb158a
buivangiaphatdev <buivangiaphat@gmail.com>
2024-07-11 23:04:53 +0700

    get all blogs

--- client/src/pages/AdminPage/BlogPage/CreateBlog/CreateBlog.jsx
+++ client/src/pages/AdminPage/BlogPage/CreateBlog/CreateBlog.jsx
@@ -5,7 +5,14 @@ import toast from 'react-hot-toast';
 import './CreateBlog.css';
 import { errorStyle, successStyle } from '../../../../utils/toast-customize';
 import { AiOutlineClose } from 'react-icons/ai';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import {
+    getDownloadURL,
+    getStorage,
+    ref,
+    uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../../../../firebase';
 
 export const CreateBlog = ({ setIsOpenCreateBlog, handleGetAllBlogs }) => {
     const [formData, setFormData] = useState({
@@ -34,11 +41,12 @@ export const CreateBlog = ({ setIsOpenCreateBlog, handleGetAllBlogs }) => {
     };
 
     const onSubmit = async (data) => {
-        const BlogData = {
+        const trimmedData = {
             ...data,
         };
         try {
-            const result = await dispatch(createBlog(BlogData));
+            const blogData = blogUrl !== '' ? { ...trimmedData, image: blogUrl } : { ...trimmedData, image: 'https://static8.depositphotos.com/1010338/959/i/450/depositphotos_9597931-stock-photo-team-gear-3d-isolated-characters.jpg' };
+            const result = await dispatch(createBlog(blogData));
             if (result.type.endsWith('fulfilled')) {
                 toast.success('Thêm blog thành công', successStyle);
             } else if (result?.error?.message === 'Rejected') {
@@ -52,83 +60,167 @@ export const CreateBlog = ({ setIsOpenCreateBlog, handleGetAllBlogs }) => {
         }
     };
 
+    const fileRef = useRef(null);
+    const [file, setFile] = useState(undefined);
+    const [filePerc, setFilePerc] = useState(0);
+    const [fileUploadError, setFileUploadError] = useState('');
+    const [blogUrl, setBlogUrl] = useState('');
+
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file);
+        }
+    }, [file]);
+
+    const handleFileUpload = (file) => {
+        if (file.size > 2 * 1024 * 1024) {
+            setFileUploadError('Dung lượng ảnh phải nhỏ hơn 2MB');
+            setFilePerc(0);
+            return;
+        }
+
+        const storage = getStorage(app);
+        const fileName = new Date().getTime() + file.name;
+        const storageRef = ref(storage, `blog/${fileName}`);
+        const uploadTask = uploadBytesResumable(storageRef, file);
+
+        uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setFilePerc(Math.round(progress));
+                setFileUploadError(''); // Reset error state on new upload
+            },
+            (error) => {
+                setFileUploadError('Tải ảnh lên thất bại');
+                setFilePerc(0); // Reset progress on error
+            },
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    setBlogUrl(downloadURL);
+                    setFilePerc(100); // Set progress to 100 on successful upload
+                    setFileUploadError(''); // Reset error state on successful upload
+                });
+            }
+        );
+    };
+
     return (
-        <div className="popup active">
-            <div className="overlay"></div>
-            <form
-                onSubmit={handleSubmit(onSubmit)}
-                className="content rounded-md p-5"
-                style={{ width: '90%', maxWidth: '600px' }}
-            >
-                <AiOutlineClose
-                    className="absolute text-sm hover:cursor-pointer"
-                    style={{ top: '10px', right: '10px' }}
-                    onClick={() => setIsOpenCreateBlog(false)}
+        <div className="popup active fixed inset-0 flex items-center justify-center z-50">
+        <div className="overlay fixed inset-0 bg-black opacity-50"></div>
+        <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="relative bg-white rounded-md shadow-lg p-6 w-11/12 max-w-xl mx-auto overflow-y-auto"
+            style={{ maxHeight: '90vh' }}
+        >
+            <AiOutlineClose
+                className="absolute text-lg hover:cursor-pointer text-gray-500 hover:text-gray-700"
+                style={{ top: '10px', right: '10px' }}
+                onClick={() => setIsOpenCreateBlog(false)}
+            />
+            <p className="text-center text-green font-bold text-2xl">
+                TẠO BLOG MỚI
+            </p>
+            <div className="form-group space-y-2">
+                <label className='font-bold'>Tiêu đề <span className="text-red-500"> * </span></label>
+                <input
+                    type="text"
+                    {...register('title', { required: 'Tiêu đề là bắt buộc' })}
+                    className="create-question-input w-full border border-gray-300 rounded-md px-3 py-2 text-center text-sm focus:ring focus:ring-blue-300"
+                    placeholder="Nhập tên của bài viết"
+                    required
                 />
-                <p className="grid text-green font-bold text-xl justify-center">
-                    TẠO BLOG MỚI
+            </div>
+            <div className="form-group space-y-2">
+                <label className='font-bold'>Tên tác giả <span className="text-red-500"> * </span></label>
+                <input
+                    type="text"
+                    {...register('blogerName', { required: 'Tên tác giả là bắt buộc' })}
+                    className="create-question-input w-full border border-gray-300 rounded-md px-3 py-2 text-center text-sm focus:ring focus:ring-blue-300"
+                    placeholder="Nhập tên của tác giả"
+                    required
+                />
+            </div>
+            <div className="form-group space-y-2">
+                <label className="font-bold">Nội Dung <span className="text-red-500"> * </span></label>
+                <textarea
+                    {...register('blogContent', { required: 'Nội dung là bắt buộc' })}
+                    className="create-question-input w-full h-[300px] border border-gray-300 rounded-md px-3 py-2 text-center text-sm focus:ring focus:ring-blue-300"
+                    required
+                />
+            </div>
+            <div className="form-group space-y-2">
+                <label className="font-bold">Tham chiếu</label>
+                <input
+                    type="text"
+                    {...register('reference')}
+                    className="create-question-input w-full border border-gray-300 rounded-md px-3 py-2 text-center text-sm focus:ring focus:ring-blue-300"
+                />
+            </div>
+            <div className="form-group space-y-2">
+                <label className="font-bold">Ảnh</label>
+                <img
+                    src={blogUrl || 'https://static8.depositphotos.com/1010338/959/i/450/depositphotos_9597931-stock-photo-team-gear-3d-isolated-characters.jpg'}
+                    className="border rounded-lg mx-auto mb-2 w-28 h-28"
+                />
+                <span
+                    className="block rounded-md bg-blue text-white p-2 mx-auto w-fit text-center hover:cursor-pointer hover:bg-blue-700"
+                    onClick={(e) => {
+                        e.preventDefault();
+                        fileRef.current.click();
+                    }}
+                >
+                    <span className='py-1 px-3 rounded-lg'>Chọn ảnh blog</span>
+                </span>
+                <input
+                    type="file"
+                    ref={fileRef}
+                    hidden
+                    onChange={(e) => {
+                        setFile(e.target.files[0]);
+                        setFileUploadError(''); // Reset error state on file change
+                    }}
+                />
+                <p className="text-sm text-center">
+                    {fileUploadError ? (
+                        <span className="text-red">
+                            {fileUploadError}
+                        </span>
+                    ) : filePerc > 0 && filePerc < 100 ? (
+                        <span className="text-gray">{`Đang tải lên ${filePerc}%`}</span>
+                    ) : filePerc === 100 ? (
+                        <span className="text-green">Tải ảnh lên thành công!</span>
+                    ) : (
+                        ''
+                    )}
                 </p>
-                <div className="form-group">
-                    <label className='font-bold'>Tiêu đề <span className="text-red"> * </span></label>
-                    <input
-                        type="text"
-                        {...register('title', { required: 'Tiêu đề là bắt buộc' })}
-                        className="create-question-input text-center text-sm"
-                        placeholder="Nhập tên của bài viết"
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label className='font-bold'>Tên tác giả <span className="text-red"> * </span></label>
-                    <input
-                        type="text"
-                        {...register('blogerName', { required: 'Tên tác giả là bắt buộc' })}
-                        className="create-question-input text-center text-sm"
-                        placeholder="Nhập tên của tác giả"
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="font-bold">Nội Dung <span className="text-red"> * </span></label>
-                    <textarea
-                        {...register('blogContent', { required: 'Nội dung là bắt buộc' })}
-                        className="create-question-input text-center text-sm"
-                        required
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="font-bold">Tham chiếu</label>
-                    <input
-                        type="text"
-                        {...register('reference')}
-                        className="create-question-input text-center text-sm"
-                    />
-                </div>
-                <div className="form-group">
-                    <label className="font-bold">Thể loại</label>
-                    <select
-                        id="categories"
-                        {...register('categories', { required: 'Phân loại là bắt buộc' })}
-                        className="create-question-input text-center text-sm"
-                    >
-                        <option value="">Select a category</option>
-                        <option value="FPT">FPT</option>
-                        <option value="Bán dẫn">Bán dẫn</option>
-                        <option value="Trí tuệ nhân tạo (AI)">Trí tuệ nhân tạo (AI)</option>
-                        <option value="Chuyển đổi số">Chuyển đổi số</option>
-                        <option value="Môi trường xanh">Môi trường xanh</option>
-                        <option value="Xe điện">Xe điện</option>
-                    </select>
-                </div>
-                <div className="flex justify-center">
-                    <button
-                        type="submit"
-                        className="block bg-primary text-white text-center rounded-md p-2 font-medium mb-1 mt-3"
-                    >
-                        Tạo blog mới
-                    </button>
-                </div>
-            </form>
-        </div>
+            </div>
+            <div className="form-group space-y-2">
+                <label className="font-bold">Thể loại</label>
+                <select
+                    id="categories"
+                    {...register('categories', { required: 'Phân loại là bắt buộc' })}
+                    className="create-question-input w-full border border-gray rounded-md px-3 py-2 text-center text-sm focus:ring focus:ring-blue-300"
+                >
+                    <option value="">Select a category</option>
+                    <option value="FPT">FPT</option>
+                    <option value="Bán dẫn">Bán dẫn</option>
+                    <option value="Trí tuệ nhân tạo (AI)">Trí tuệ nhân tạo (AI)</option>
+                    <option value="Chuyển đổi số">Chuyển đổi số</option>
+                    <option value="Môi trường xanh">Môi trường xanh</option>
+                    <option value="Xe điện">Xe điện</option>
+                </select>
+            </div>
+            <div className="flex justify-center">
+                <button
+                    type="submit"
+                    className="bg-primary text-white text-center rounded-md px-4 py-2 font-medium hover:bg-blue-700"
+                >
+                    Tạo blog mới
+                </button>
+            </div>
+        </form>
+    </div>
+    
     );
 };
--- client/src/pages/Blog/Blog.jsx
+++ client/src/pages/Blog/Blog.jsx
@@ -1,42 +1,57 @@
 import { FaLongArrowAltRight } from "react-icons/fa";
 import { FaCircleArrowRight } from "react-icons/fa6";
 import '../Blog/Blog.css'
-import { LuDot } from "react-icons/lu";
-import { IoAddCircleSharp } from "react-icons/io5";
+
+import { useEffect, useState } from "react";
+import { getAllBlogs } from "../../features/blogs/blogSlice";
+import { useDispatch } from "react-redux";
+import { formatDate } from "../../utils/format";
 
 export const Blog = () => {
-    return (
-        <div>
+    const dispatch = useDispatch();
+    const [blogs, setBlog] = useState([]);
+    console.log(blogs);
 
-            <div className="grid grid-cols-3 gap-6 p-6">
+    async function initialBlogList() {
+        let output = await dispatch(getAllBlogs());
+
+        setBlog(output.payload);
+    }
+    useEffect(() => {
+        initialBlogList();
+    }, [])
+    return (
+        <div className="mt-10">
+            <div className="grid grid-cols-3 gap-6 p-6 mb-10 ">
                 <div className="col-span-2 max-h-96">
                     <div className="flex items-center p-6 rounded-lg bg-white">
-                        <h1 className="text-5xl font-bold text-gray-800 italic">
+                        <h1 className="text-5xl font-bold text-black italic">
                             Tốt nhất trong tuần
                         </h1>
-                        <div className="flex items-center mt-5  text-blue-500 hover:text-blue-800 cursor-pointer ml-4">
-                            <p className="text-sm mr-2">Xem tất cả</p>
+                        <div className="flex items-center mt-5  text-blue hover:text-primary cursor-pointer ml-4">
+                            <p className="text-base mr-2">Xem tất cả</p>
                             <FaLongArrowAltRight className="text-2xl" />
                         </div>
                     </div>
+
                     <div className="overflow-hidden relative mt-4 rounded-lg bg-gray-100 shadow-md">
                         <img
-                            className="w-full h-[570px] rounded-xl object-cover"
-                            src="https://img.freepik.com/free-photo/black-painted-wall-textured-background_53876-110728.jpg"
+                            className="w-full max-h-[470px] rounded-xl object-cover"
+                            src={blogs[0]?.image}
                             alt="Travel Pro"
                         />
                         <div className="absolute top-10 left-10 w-28 bg-white rounded-full px-3 py-1 text-sm font-semibold text-gray-700">
-                            Sep 06, 2022
+                            {formatDate(blogs[0]?.createdAt)}
                         </div>
                         <div className="absolute top-20 left-10 w-28 text-white text-center border border-white rounded-full px-3 py-1 text-sm font-semibold">
-                            Travel
+                            {blogs[0]?.categories}
                         </div>
-                        <div className="absolute top-20 right-10 bg-white bg-opacity-90 rounded-lg p-4">
-                            <div className="text-sm font-semibold text-gray-700 mb-1">
-                                Travel
+                        <div className="absolute top-20 right-10 bg-white bg-opacity-65 rounded-lg p-4">
+                            <div className="text-sm font-semibold text-gray mb-1">
+                                {blogs[0]?.categories}
                             </div>
                             <div className="w-80 text-2xl font-bold text-black">
-                                Get to your dream destinations now with Travel Pro
+                                {blogs[0]?.title}
                             </div>
                         </div>
                         <div className="rotate-[-45deg] absolute bottom-4 right-4 bg-white bg-opacity-70 rounded-full p-2 mr-2 mb-2">
@@ -44,72 +59,62 @@ export const Blog = () => {
                         </div>
                     </div>
                 </div>
-                <div className="grid grid-rows-2 gap-4 mt-6 ">
+                <div className="grid grid-rows-2 gap-4 mt-16 ">
                     <div className="rounded-lg bg-white shadow-md">
                         <img
                             className="rounded-xl w-full h-full object-cover"
-                            src="https://data.vietchem.com.vn/labvietchem/2021/11/mau-ngoc-lam-la-mau-gi.jpg"
+                            src={blogs[1]?.image}
                             alt="Image 1"
                         />
-                        <div className="flex rounded-full absolute border border-black
-                     text-black top-28 ml-8 justify-center items-center w-fit pr-2  ">
-                            <LuDot /> ADS
-                        </div>
-                        <IoAddCircleSharp className="absolute text-black top-24 right-10 w-12 h-12" />
-                        <p className="block font-semibold absolute top-36 text-lg ml-8">
-                            Become A
-                            <span className="block">BROADCAST MEMBER</span>
+
+                        <p className="block p-4 mr-10 bg-white bg-opacity-65 rounded-lg font-bold absolute top-36 text-xl text-black ml-8">
+                            {blogs[1]?.title}
                         </p>
-                        <p className=" block font-bold text-3xl absolute top-48 ml-8 mt-5">Real talk in a
-                            <span className="block"> corporate world </span> </p>
-                        <a className="absolute top-80 mt-10 right-20 text-base font-bold hover:text-blue-700">Xem thêm</a>
+
+                        <a className="absolute top-64 mt-20 right-20 text-base font-bold bg-light_gray pl-2 pr-2 rounded-full hover:bg-primary text-black">Xem thêm</a>
 
                     </div>
                     <div className="rounded-lg bg-white shadow-md">
                         <img
                             className="rounded-xl w-full h-full object-cover"
-                            src="https://img.freepik.com/free-photo/black-painted-wall-textured-background_53876-110728.jpg"
+                            src={blogs[2]?.image}
                             alt="Image 2"
                         />
-                        <div className="absolute bottom-56 right-10 w-12 h-12 rounded-full flex items-center justify-center border border-white">
-                            <p className="text-white font-bold">24</p>
-                        </div>
-                        <div className="flex items-center absolute bottom-4 right-36 bg-gray-300 hover:bg-slate-50 h-16 px-4 py-2 rounded-full shadow-lg cursor-pointer">
-                            <span className="mr-2 font-medium text-black">
+
+                        <p className="block p-4 mr-10 bg-white bg-opacity-65 rounded-lg font-bold absolute bottom-40 text-xl text-black ml-8">
+                            {blogs[2]?.title}
+                        </p>
+
+                        <div className="flex items-center absolute bottom-16 bg-light_gray hover:bg-primary right-14 pl-2 pr-2 rounded-full cursor-pointer transition duration-300 ease-in-out">
+                            <span className="flex items-center mr-2 font-medium text-base text-black transition duration-300 ease-in-out">
                                 Xem tất cả bài viết
+                                <FaLongArrowAltRight className="ml-2 text-2xl text-black transition duration-300 ease-in-out" />
                             </span>
-                            <FaLongArrowAltRight className="text-2xl text-black" />
                         </div>
+
                     </div>
                 </div>
             </div>
 
-            <div class="max-w-7xl  mx-auto ">
-                <div class="flex items-start p-4 border border-gray-200 bg-white shadow-lg rounded-lg mb-4">
-                    <img class="w-60 h-48 object-cover rounded-lg mr-4" src="https://data.vietchem.com.vn/labvietchem/2021/11/mau-ngoc-lam-la-mau-gi.jpg" alt="Image 3" />
-                    <div class="flex flex-col justify-between">
-                        <div>
-                            <span class="text-sm text-gray-500">1 phút trước</span>
-                            <strong class="block text-lg font-semibold mt-1">Điện thoại gập Samsung Galaxy Z Fold 5 ra mắt: Thiết kế siêu mỏng siêu nhẹ, tích hợp AI "xịn xò", giá bán lên đến 55 triệu đồng</strong>
+
+
+            {blogs?.map((blog) => {
+                return (
+                    <div class="max-w-7xl  mx-auto ">
+                        <div class="flex items-start p-4 border border-gray-200 bg-white shadow-lg rounded-lg mb-4">
+                            <img class="w-60 h-48 object-cover rounded-lg mr-4" src={blog?.image} alt="Image 3" />
+                            <div class="flex flex-col justify-between">
+                                <div>
+                                    <span class="text-sm text-gray-500">1 phút trước</span>
+                                    <strong class="block text-lg font-semibold mt-1">{blog?.title}</strong>
+                                </div>
+                                <span class="text-blue-500 cursor-pointer hover:underline mt-2">Xem thêm</span>
+                            </div>
                         </div>
-                        <span class="text-blue-500 cursor-pointer hover:underline mt-2">Xem thêm</span>
+
                     </div>
-                </div>
-
-                <div class="flex items-start p-4 border border-gray-200 bg-white shadow-lg rounded-lg">
-                    <img class="w-60 h-48 object-cover rounded-lg mr-4" src="https://data.vietchem.com.vn/labvietchem/2021/11/mau-ngoc-lam-la-mau-gi.jpg" alt="Image 3" />
-                    <div class="flex flex-col justify-between">
-                        <div>
-                            <span class="text-sm text-gray-500">1 phút trước</span>
-                            <strong class="block text-lg font-semibold mt-1">Điện thoại gập Samsung Galaxy Z Fold 5 ra mắt: Thiết kế siêu mỏng siêu nhẹ, tích hợp AI "xịn xò", giá bán lên đến 55 triệu đồng</strong>
-                        </div>
-                        <span class="text-blue-500 cursor-pointer hover:underline mt-2">Xem thêm</span>
-                    </div>
-                </div>
-             
-            </div>
-
-
+                )
+            })}
         </div>
     );
 };
