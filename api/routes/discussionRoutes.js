3c0be304890bc6d3fc1d2f134abe13fbe92e506f
buivangiaphatdev <buivangiaphat@gmail.com>
2024-07-11 14:54:31 +0700

    crud discussion

--- api/routes/discussionRoutes.js
+++ api/routes/discussionRoutes.js
@@ -1,17 +1,21 @@
 const express = require('express');
-const { createDiscussion, getAllDiscussion, updateDiscussion, deleteDiscussion, getDiscussionById } = require('../controllers/discussionController');
+const { createDiscussion,
+    getAllDiscussion,
+    updateDiscussion,
+    deleteDiscussion,
+    getDiscussionById } = require('../controllers/discussionController');
 const { protect } = require('../middleware/accountMiddleware');
 const router = express.Router();
 
 
 router.route('/').get(getAllDiscussion);
 router.route('/post')
-.post(protect, createDiscussion)
+    .post(protect, createDiscussion)
 
 router.route('/post/:discussionId')
-.get(getDiscussionById)
-.patch(protect, updateDiscussion)
-.delete(protect, deleteDiscussion)
+    .get(getDiscussionById)
+    .patch(protect, updateDiscussion)
+    .delete(protect, deleteDiscussion)
 
 
 
